2853be2f9eaee9f483bc24112605b753b54fb6e1
NippyCoder <n.hosny@boltsmartsolutions.com>
2020-12-29 15:48:10 +0200

    about team animation
    added display/hide animation for teams

--- src/app/modules/about-us/about-team/about-team.component.ts
+++ src/app/modules/about-us/about-team/about-team.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutTeamComponent implements OnInit {
 
+  //Animation
+  animation: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.animation = ''
   }
 
   DisplayTeam(selector) {
+    if (this.animation == '') {
+      this.animation = 'displayTeam'
+    }
+    else if (this.animation == 'displayTeam') {
+      this.animation = 'hideTeam'
+    }
     if (selector == "tech") {
       document.getElementById('dynamicTeam').style.display = 'block';
     }
