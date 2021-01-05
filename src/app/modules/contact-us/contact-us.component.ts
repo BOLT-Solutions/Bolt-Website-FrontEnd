c005e04c2f11bbc0f18d1ad6504df261e0c97b59
moataz yassin <m.yassin@boltsmartsolutions.com>
2021-01-05 18:24:20 +0200

    Fixed Dropdown

--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -12,6 +12,8 @@ export class ContactUsComponent implements OnInit {
   showOptions: boolean = false;
   show: boolean = true;
   stValue: any;
+  ItemValue: any;
+
   status_values = [
   { id: 0, value: "In Progress" },
   { id: 1, value: "Completed" },
@@ -38,5 +40,14 @@ export class ContactUsComponent implements OnInit {
     this.show = true;
     this.flag = false;
   }
+  getItemValue(id: any) {
+    this.ItemValue = this.status_values.find(b => b.id == id);
+    console.log("status value ", this.ItemValue);
+    this.stValue = this.ItemValue.value;
+    this.showOptions = false;
+    this.show = true;
+
+  }
+
 
 }
